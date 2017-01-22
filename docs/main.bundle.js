5c20b32e63515c871cc96fcd3e923486db76a70f
AIRTucha <airtucha@yandex.ru>
2017-01-22 22:56:01 +0200

    production mode turned on

--- docs/main.bundle.js
+++ docs/main.bundle.js
@@ -26,28 +26,35 @@ var VKService = (function () {
      * @return - array of friends with brief data
      */
     VKService.getFriends = function (id) {
-        var users = [
-            {
-                first_name: "Артем",
-                last_name: "Матюшевский",
-                photo_50: "https://pp.vk.me/c837327/v837327423/13f3e/k6AH4m_xU4g.jpg",
-                uid: 333423
-            },
-            {
-                first_name: "Nancy",
-                last_name: "Novikova",
-                photo_50: "https://pp.vk.me/c626522/v626522377/20bc4/Q1CAaYRscKk.jpg",
-                uid: 1366377
-            },
-            {
-                first_name: "Кайрат",
-                last_name: "Сагинаев",
-                photo_50: "https://pp.vk.me/c626231/v626231924/46c7f/rhs6iaW_ChY.jpg",
-                uid: 1442924
-            },
-        ];
+        // let users: Array<any> = [
+        //   {
+        //     first_name : "Артем",
+        //     last_name : "Матюшевский",
+        //     photo_50 : "https://pp.vk.me/c837327/v837327423/13f3e/k6AH4m_xU4g.jpg",
+        //     uid : 333423
+        //   },
+        //   {
+        //     first_name : "Nancy",
+        //     last_name : "Novikova",
+        //     photo_50 : "https://pp.vk.me/c626522/v626522377/20bc4/Q1CAaYRscKk.jpg",
+        //     uid : 1366377
+        //   },
+        //   {
+        //     first_name : "Кайрат",
+        //     last_name : "Сагинаев",
+        //     photo_50 : "https://pp.vk.me/c626231/v626231924/46c7f/rhs6iaW_ChY.jpg",
+        //     uid : 1442924
+        //   },
+        // ]
         return new Promise(function (resolve) {
-            return resolve(users.map(function (v) { return new __WEBPACK_IMPORTED_MODULE_1__user__["a" /* User */](v.uid, v.first_name, v.last_name, v.photo_50); }));
+            // resolve( users.map( v => new User(v.uid, v.first_name, v.last_name, v.photo_50) ))
+            return VK.api('friends.get', {
+                user_id: id,
+                order: "hint",
+                fields: "photo_50"
+            }, function (r) {
+                resolve(r.response.map(function (v) { return new __WEBPACK_IMPORTED_MODULE_1__user__["a" /* User */](v.uid, v.first_name, v.last_name, v.photo_50); }));
+            });
         });
     };
     /**
@@ -68,11 +75,18 @@ var VKService = (function () {
      */
     VKService.getUsers = function (id) {
         return new Promise(function (resolve) {
-            return resolve(new __WEBPACK_IMPORTED_MODULE_1__user__["a" /* User */]("TestId", //r.response[0].uid, 
-            "TestName", //r.response[0].first_name, 
-            "TestLastMame", // r.response[0].last_name, 
-            "https://pp.vk.me/c626231/v626231924/46c7f/rhs6iaW_ChY.jpg" //r.response[0].photo_50
-            ));
+            // resolve( new User(
+            //         "TestId",//r.response[0].uid, 
+            //         "TestName",//r.response[0].first_name, 
+            //         "TestLastMame",// r.response[0].last_name, 
+            //         "https://pp.vk.me/c626231/v626231924/46c7f/rhs6iaW_ChY.jpg"//r.response[0].photo_50
+            //         ))   
+            return VK.api('users.get', {
+                user_ids: id,
+                fields: "photo_50"
+            }, function (r) {
+                resolve(new __WEBPACK_IMPORTED_MODULE_1__user__["a" /* User */](r.response[0].uid, r.response[0].first_name, r.response[0].last_name, r.response[0].photo_50));
+            });
         });
     };
     /**
@@ -80,28 +94,35 @@ var VKService = (function () {
      * @return - array of friends with brief data
      */
     VKService.getSearch = function (query) {
-        var users = [
-            {
-                first_name: "Артемка",
-                last_name: "Матюшевский",
-                photo_50: "https://pp.vk.me/c837327/v837327423/13f3e/k6AH4m_xU4g.jpg",
-                uid: 3334231
-            },
-            {
-                first_name: "Nancys",
-                last_name: "Novikova",
-                photo_50: "https://pp.vk.me/c626522/v626522377/20bc4/Q1CAaYRscKk.jpg",
-                uid: 13663772
-            },
-            {
-                first_name: "Кайрат",
-                last_name: "Сагинаев",
-                photo_50: "https://pp.vk.me/c626231/v626231924/46c7f/rhs6iaW_ChY.jpg",
-                uid: 14429243
-            },
-        ];
+        // let users: Array<any> = [
+        //   {
+        //     first_name : "Артемка",
+        //     last_name : "Матюшевский",
+        //     photo_50 : "https://pp.vk.me/c837327/v837327423/13f3e/k6AH4m_xU4g.jpg",
+        //     uid : 3334231
+        //   },
+        //   {
+        //     first_name : "Nancys",
+        //     last_name : "Novikova",
+        //     photo_50 : "https://pp.vk.me/c626522/v626522377/20bc4/Q1CAaYRscKk.jpg",
+        //     uid : 13663772
+        //   },
+        //   {
+        //     first_name : "Кайрат",
+        //     last_name : "Сагинаев",
+        //     photo_50 : "https://pp.vk.me/c626231/v626231924/46c7f/rhs6iaW_ChY.jpg",
+        //     uid : 14429243
+        //   },
+        // ]
         return new Promise(function (resolve) {
-            return resolve(users.map(function (v) { return new __WEBPACK_IMPORTED_MODULE_1__user__["a" /* User */](v.uid, v.first_name, v.last_name, v.photo_50); }));
+            //  resolve( users.map( v => new User(v.uid, v.first_name, v.last_name, v.photo_50) ))
+            return VK.api('users.search', {
+                q: query,
+                count: 5,
+                fields: "photo_50"
+            }, function (r) {
+                resolve(r.response.slice(1).map(function (v) { return new __WEBPACK_IMPORTED_MODULE_1__user__["a" /* User */](v.uid, v.first_name, v.last_name, v.photo_50); }));
+            });
         });
     };
     VKService = __decorate([
